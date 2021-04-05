bc0cbe819f05a785fa6ef1afd5922cfd375f6ac5
myFirstreact <rajgaykar95@gmail.com>
2021-04-05 12:26:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from'react'
+import './App.css';
+import  Datafetching from './components/Datafetching' 
+
+
+function App() {
+  return (
+    <div className="App">
+      <Datafetching />
+      </div>
+  );
+}
+
+export default App;
